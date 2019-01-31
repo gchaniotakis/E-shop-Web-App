14a27d3a89331fcd2ec5629723f4205e430f61c3
George Chaniotakis <haniotinho@gmail.com>
2019-01-31 18:54:05 +0200

    added more html pages + entity framework

---
+++ eshopapp/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace eshopapp.DAL
+{
+    public class ApplicationDbContext : System.Data.Entity.DbContext
+    {
+        System.Data.Entity.DbSet <Models.User> Users { get; set; }
+    }
+}
